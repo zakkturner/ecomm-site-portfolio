e108084bcf86fe1cbdf4fe8aeaeeb2026be694ca
zakkturner <30674356+zakkturner@users.noreply.github.com>
2020-05-19 15:41:52 -0400

    created shop page

--- src/App.js
+++ src/App.js
@@ -2,20 +2,14 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
-
-const HatsPage = props => (
-  <div>
-    <h1>Hats Page</h1>
-    <button onClick={() => props.history.push("/")}>Back</button>
-  </div>
-);
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route exact path="/hats" component={HatsPage} />
+        <Route exact path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
